60487b06c637348befa0d321bff507c3b9fb7543
Daryll Wong <daryll_wong@hotmail.com>
2019-12-05 20:20:17 +0800

    remove top action bar title navigation

--- app/src/main/java/com/example/hungrytogetherandroidapplication/open_orders/OpenOrdersFragment.java
+++ app/src/main/java/com/example/hungrytogetherandroidapplication/open_orders/OpenOrdersFragment.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -46,6 +47,13 @@ public class OpenOrdersFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
