29b0527f8d21b2f486ecb35606a9b254e13d3b27
evangeline-lim <evangeline_lim@mymail.sutd.edu.sg>
2019-12-06 07:58:18 +0800

    add firebase write

--- app/src/main/java/com/example/hungrytogetherandroidapplication/ordering_portal/OrderingPortalActivity.java
+++ app/src/main/java/com/example/hungrytogetherandroidapplication/ordering_portal/OrderingPortalActivity.java
@@ -16,6 +16,7 @@ import com.example.hungrytogetherandroidapplication.R;
 import com.example.hungrytogetherandroidapplication.main_activity_portal.MainActivity;
 import com.example.hungrytogetherandroidapplication.my_orders.MyOrderItem;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
@@ -27,20 +28,38 @@ import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firestore.v1.StructuredQuery;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class OrderingPortalActivity extends AppCompatActivity {
 
     private RadioGroup radioFoodGroup;
     private RadioButton radioFoodButton;
     private Button completeOrder;
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
-
     Button completeorderButton;
+
+    // Get current user's fb uid, store in fbUid string object
+    private FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    private String fbUid = user.getUid();
+    private String sailor_name = user.getDisplayName();
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ordering_portal);
+
+        // Retrieve the OpenOrder doc id that I'm writing into
+        Intent fromOpenOrdersIntent=getIntent();
+        String orderIdString= fromOpenOrdersIntent.getStringExtra("order_id");
+        Log.d("Ordering portal", "selected order id: "+orderIdString);
+        Log.d("Ordering portal", "my fbuid: "+fbUid);
+        Log.d("Ordering portal", "sailor name: "+sailor_name);
+
+        // Obtain mySailorOrderRef, the docref to where I'm supposed to write to (save my new sailor order here)
+        final DocumentReference mySailorOrderRef = db.collection("OpenOrders").document(orderIdString).collection("SailorOrders").document(fbUid);
+
 
         // this is your activity.
         // the layout is activity_ordering_portal
@@ -70,10 +89,13 @@ public class OrderingPortalActivity extends AppCompatActivity {
                 //1) send data(label/name of food, cost and userid) to firestore
                 //get selected radio button from radioGroup
                 //find the radio button by returned id
-                int selectedId = radioFoodGroup.getCheckedRadioButtonId();
-                radioFoodButton = (RadioButton) findViewById(selectedId);
 
-                String food = (String) radioFoodButton.getText();
+//                int selectedId = radioFoodGroup.getCheckedRadioButtonId();
+//                radioFoodButton = (RadioButton) findViewById(selectedId);
+//
+//                String food = (String) radioFoodButton.getText();   //obtained meal_label!!
+
+
 
                 //sending up to firebase
 //                final String openorderid = "pdQwKDYLtmCoO1PK54MF";// open order id currently hardcoded for testing
@@ -89,22 +111,44 @@ public class OrderingPortalActivity extends AppCompatActivity {
 //
 //                Toast.makeText(OrderingPortalActivity.this, "Write Attempt", Toast.LENGTH_SHORT).show();
 
+                Map<String, Object> order = new HashMap<>();
+                order.put("meal_cost", "Los Angeles");
+                order.put("meal_label", "Los Angeles");
+                order.put("sailor_name", "Los Angeles");
+                order.put("total_cost", "Los Angeles");
 
-                db.collection("OpenOrder")
-                        .get()
-                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                mySailorOrderRef.set(order)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
                             @Override
-                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                if (task.isSuccessful()) {
-                                    for (QueryDocumentSnapshot document : task.getResult()) {
-                                        Log.d("FIRESTORE", document.getId() + " => " + document.getData());
-                                        Toast.makeText(OrderingPortalActivity.this, document.getData().toString(), Toast.LENGTH_SHORT).show();
-                                    }
-                                } else {
-                                    Log.d("FIRESTORE", "Error getting documents: ", task.getException());
-                                }
+                            public void onSuccess(Void aVoid) {
+                                Log.d("evange", "DocumentSnapshot successfully written!");
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w("evange", "Error writing document", e);
                             }
                         });
+
+
+
+
+//                db.collection("OpenOrder")
+//                        .get()
+//                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                            @Override
+//                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                                if (task.isSuccessful()) {
+//                                    for (QueryDocumentSnapshot document : task.getResult()) {
+//                                        Log.d("FIRESTORE", document.getId() + " => " + document.getData());
+//                                        Toast.makeText(OrderingPortalActivity.this, document.getData().toString(), Toast.LENGTH_SHORT).show();
+//                                    }
+//                                } else {
+//                                    Log.d("FIRESTORE", "Error getting documents: ", task.getException());
+//                                }
+//                            }
+//                        });
 
 
                 //get intent to switch to next activity
