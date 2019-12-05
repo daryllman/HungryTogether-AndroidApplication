45220cbc2cf293eb08571657e1958d79a2534d38
evangeline-lim <evangeline_lim@mymail.sutd.edu.sg>
2019-12-06 06:42:07 +0800

    change myprofile ui

--- app/src/main/java/com/example/hungrytogetherandroidapplication/my_profile/MyProfileFragment.java
+++ app/src/main/java/com/example/hungrytogetherandroidapplication/my_profile/MyProfileFragment.java
@@ -42,6 +42,7 @@ public class MyProfileFragment extends Fragment {
     TextView idField;
     ImageView photoField;
     TextView fbUid;
+    Button sign_out;
 
 
     public MyProfileFragment() {
@@ -58,11 +59,8 @@ public class MyProfileFragment extends Fragment {
         // Initialise all the variables
         nameField = fragmentView.findViewById(R.id.test_name);
         emailField = fragmentView.findViewById(R.id.test_email);
-        idField = fragmentView.findViewById(R.id.test_id);
         photoField = fragmentView.findViewById(R.id.test_photo);
-        fbUid = fragmentView.findViewById(R.id.test_firebaseUid);
-
-
+        sign_out = fragmentView.findViewById(R.id.log_out);
 
         // Configure sign-in to request the user's ID, email address, and basic
         // profile, ID and basic profile are included in DEFAULT_SIGN_IN.
@@ -82,15 +80,15 @@ public class MyProfileFragment extends Fragment {
             //String personGivenName = acct.getGivenName();
             //String personFamilyName = acct.getFamilyName();
             String personEmail = acct.getEmail();
-            String personId = acct.getId();
+//            String personId = acct.getId();
             Uri personPhotoURL = acct.getPhotoUrl(); //URL that contains the photo file of account
             FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
             // Show the account details on this page~
-            nameField.setText("Name: "+ personName);
-            emailField.setText("Email: "+ personEmail);
-            idField.setText("ID: "+ personId);
-            fbUid.setText("fb UID:" + user.getUid());
+            nameField.setText(personName);
+            emailField.setText(personEmail);
+//            idField.setText("ID: "+ personId);
+//            fbUid.setText("fb UID:" + user.getUid());
 
 
             // Glide is a module to load and cache images (read the build.gradle file for more info that i added)
@@ -99,12 +97,31 @@ public class MyProfileFragment extends Fragment {
                     .load(personPhotoURL).into(photoField); //load photo from url(personPhotoURL) into the photoField
         }
 
-
-
+        // Create Listener for sign out button
+        sign_out.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "sign out", Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
         return fragmentView;
     }
 
+//    private void signOut() {
+//
+//        mGoogleSignInClient.signOut() // use google sign in client to sign out officially...
+//                .addOnCompleteListener(getActivity(), new OnCompleteListener<Void>() { // Adds a listener that is called when the Task completes. //The listener will be called on main application thread. If the Task is already complete, a call to the listener will be immediately scheduled. If multiple listeners are added, they will be called in the order in which they were added.
+//                    @Override
+//                    public void onComplete(@NonNull Task<Void> task) { // Execute this onComplete() func when listener (OnCompleteListener) listens that task is completed // Note: task is never null as the task here is a completed task
+//                        Toast.makeText(getActivity(), "You have signed out successfully", Toast.LENGTH_SHORT).show();
+//                        FirebaseAuth.getInstance().signOut();
+//                        startActivity(new Intent(getActivity(), MainLogin.class)); // Jump to main login screen once sign out is successfull
+//                        finish();
+//                    }
+//                });
 
-}
+
+    }
+
